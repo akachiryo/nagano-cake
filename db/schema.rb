8f293af35e745cf43984360a59011fd801e59399
akachiryo <rirugameri@icloud.com>
2021-08-14 17:05:26 +0000

    商品新規作成、詳細、編集　機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_14_144715) do
+ActiveRecord::Schema.define(version: 2021_08_14_165704) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -50,9 +50,9 @@ ActiveRecord::Schema.define(version: 2021_08_14_144715) do
     t.string "image_id", null: false
     t.text "introduction", null: false
     t.integer "price", null: false
+    t.boolean "is_active", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "is_active"
     t.index ["genre_id"], name: "index_items_on_genre_id"
   end
 
