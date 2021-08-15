be788e458d8e0c7aea5b2aa3e2863c23186562da
nishimototo <dkspm2436@gmail.com>
2021-08-15 23:23:26 +0000

    customerモデルのvalidation修正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,15 +4,13 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-　validates :last_name, presence: true
+  validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
   validates :postcode, presence: true
   validates :address, presence: true
   validates :phone_number, presence: true
-
-
 
   has_many :addresses, dependent: :destroy
 
