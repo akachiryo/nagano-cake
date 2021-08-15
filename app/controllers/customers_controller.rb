3e68a39c4c1dccb94e22b1b2a81444f3242181a5
nishimototo <dkspm2436@gmail.com>
2021-08-15 08:55:56 +0000

    アドミ、カスタマー両方の退会機能実装

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,30 +1,30 @@
 class CustomersController < ApplicationController
   #before_action :authenticate_customer!
-  before_action :ensure_correct_user
+  before_action :ensure_correct_customer, only: [:show, :edit]
 
   def show
-
+    @customer = Customer.find(params[:id])
   end
 
   def edit
-
+    @customer = Customer.find(params[:id])
   end
 
   def update
-
+    @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
-      redirect_to customer_path(current_customer), notice: "会員情報を更新しました"
+      redirect_to customer_path(@customer), notice: "会員情報を更新しました"
     else
       render :edit
     end
   end
 
   def unsubscribe
-
+    @customer = Customer.find(params[:id])
   end
 
   def withdraw
-
+    @customer = Customer.find(params[:id])
     @customer.update(is_deleted: true)
     reset_session
     redirect_to root_path
@@ -35,10 +35,10 @@ class CustomersController < ApplicationController
       params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number, :email, :is_deleted)
     end
 
-    def ensure_correct_user
+    def ensure_correct_customer
       @customer = Customer.find(params[:id])
       if @customer != current_customer
-        redirect_to root_path
+         redirect_to root_path
       end
     end
 end
