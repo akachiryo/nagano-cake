f2f82df1f175caba930f7c1b32a98a69210b2d5a
nishimototo <dkspm2436@gmail.com>
2021-08-16 00:28:31 +0000

    addressのcreateアクション修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,5 @@
 class Address < ApplicationRecord
-  validates :postcode, presence: true
+  validates :postcode, presence: true, format: { with: /\A\d{7}\z/ } #郵便番号ハイフンなし7桁
   validates :address, presence: true
   validates :name, presence: true
 
