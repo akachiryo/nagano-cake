8fb47aa9eb3c14f29ede3a277cfbb4fafb0ea475
akachiryo <rirugameri@icloud.com>
2021-08-16 14:38:33 +0000

    admin 顧客一覧レイアウト途中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_out_path_for(resource_or_scope)   #アドミのログアウト後はアドミのログインへ
+  def after_sign_out_path_for(resource_or_scope)
+    #アドミのログアウト後はアドミのログインへ
     if resource_or_scope == :admin
         new_admin_session_path
     else
-        root_path　　　　　　　　　　　　　　　　　#アドミ以外（会員）のログアウト後はルートへ
+        root_path
+        #アドミ以外（会員）のログアウト後はルートへ
     end
   end
 
