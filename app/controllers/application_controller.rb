50aa4cf1c3742f674edfda8cb89dd4d2801061d9
nishimototo <dkspm2436@gmail.com>
2021-08-16 05:12:55 +0000

    会員登録画面のレイアウトはじめ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_out_path_for(resource_or_scope)   #アドミのログアウト後はアドミのログインへ
+  def after_sign_out_path_for(resource_or_scope)
     if resource_or_scope == :admin
-        new_admin_session_path
+      new_admin_session_path
     else
-        root_path　　　　　　　　　　　　　　　　　#アドミ以外（会員）のログアウト後はルートへ
+      root_path
     end
   end
 
