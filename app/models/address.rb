e4d97530838e9022301054b22bbe9209dc869f42
akachiryo <rirugameri@icloud.com>
2021-08-18 14:03:41 +0000

    切り替え

--- app/models/address.rb
+++ app/models/address.rb
@@ -5,6 +5,9 @@ class Address < ApplicationRecord
   validates :postcode, presence: true, format: { with: /\A\d{7}\z/ } #郵便番号ハイフンなし7桁
   validates :address, presence: true
   validates :name, presence: true
-
+  
+  def view_address
+    "〒" + self.postcode + "　" + self.address + "　" + self.name
+  end
   
 end
